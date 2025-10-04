9c7853b50c122d3fad88d7179743db3a9d9ed2a2
Damian Kocjan <DamianoKPL@wp.pl>
2025-10-04 11:07:54 +0200

    feat: add simple blog controller

---
+++ Web/Web/Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        private static readonly List<BlogArticleViewModel> _articles = new()
+        {
+            new BlogArticleViewModel() { Title = "First Post", Description = "This is the first post on my blog." },
+            new BlogArticleViewModel() { Title = "Second Post", Description = "This is the second post on my blog." },
+            new BlogArticleViewModel() { Title = "Third Post", Description = "This is the third post on my blog." }
+        };
+
+        public IActionResult Article(int id)
+        {
+            var article = _articles.FirstOrDefault(a => a.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+            return View(article);
+        }
+
+        public IActionResult Index()
+        {
+            return View(_articles);
+        }
+    }
+}
